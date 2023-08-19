b778365c94ec89d4ee804b8828edfed8f0898773
generalpancakemstr <generalpancake@gmail.com>
2023-08-19 09:47:04 -0600

    Total rewrite, trying to figure out how to do this. Starting point for a Symbol struct.

--- dol_systems/src/main.rs
+++ dol_systems/src/main.rs
@@ -1,34 +1,69 @@
-use dol_systems::{Predecessor, Successor, Productions, DOLString}; 
-use std::collections::HashMap; 
-use std::rc::Rc; 
+// use dol_systems::{Predecessor, Successor, Productions, DOLString}; 
+// use std::collections::HashMap; 
+// use std::rc::Rc; 
+
+// use dol_systems::{State, Instruction}; 
+
+#[derive(Debug)]
+struct State(String); 
+
+struct Instruction(
+    Box<dyn Fn(& State, Vec<f64>) -> State>
+);
+
+impl Instruction {
+    fn evaluate(&self, state: & State, parameters: Vec<f64> ) -> State {
+        self.0(&state, parameters)
+    }
+}
+
+struct Symbol {
+    representation: String,
+    command: Instruction
+}
+
+
 
 fn main() {
     // G = <V, w, P> 
-    // DOL-System = <Alphabet, Axiom, Productions>     
+    // DOL-System = <Alphabet, Axiom, Productions>
 
-    // This is now a unit test (anabaena_catenula_filament)
+    // let a = Predecessor("A",instruction);
 
-    let a_r = Rc::new(Predecessor(String::from("a_r"))); 
-    let a_l = Rc::new(Predecessor(String::from("a_l"))); 
-    let b_r = Rc::new(Predecessor(String::from("b_r"))); 
-    let b_l = Rc::new(Predecessor(String::from("b_l"))); 
+    // let predecessor = Symbol{repr: String::from("A"),command: String::from("Move forward by 1")}; 
+    // println!("{}",predecessor.repr); 
+    // println!("{}",predecessor.command); 
 
-    let mut productions = Productions(HashMap::new()); 
-    productions.0.insert(Rc::clone(&a_r),Successor(vec![Rc::clone(&a_l),Rc::clone(&b_r)]));
-    productions.0.insert(Rc::clone(&a_l),Successor(vec![Rc::clone(&b_l),Rc::clone(&a_r)]));
-    productions.0.insert(Rc::clone(&b_r),Successor(vec![Rc::clone(&a_r)]));
-    productions.0.insert(Rc::clone(&b_l),Successor(vec![Rc::clone(&a_l)]));
 
-    let mut axiom = DOLString(vec![Rc::clone(&a_r)]);
+    // Symbol.instruction.evaluate(&state, parameters); 
+    // let command = Command(Box::new(actual_instruction));
 
-    println!("0 {}",axiom);
+    let s = State(String::from("An Initial State"));
+    let p = vec![0.0,1.0,2.0]; 
 
-    for n in 1..5 {        
-        // println!("{n} {:?}",axiom);        
-        axiom = productions.evaluate(&axiom);
-        println!("{n} {}",axiom);
-    };  
+    let predecessor_a = Symbol{representation:String::from("A"),command:Instruction(Box::new(actual_instruction))};
 
-    println!("{}",axiom);
+    println!("Predecessor symbol: {}",predecessor_a.representation);
+    let new_state = predecessor_a.command.evaluate(&s, p); 
+    println!("{:?}",new_state);
+
+    let p = vec![3.0,4.0,5.0]; 
+    let new_state = predecessor_a.command.evaluate(&new_state, p); 
+    println!("{:?}",new_state);
 
 }
+
+fn actual_instruction(state: & State, parameters: Vec<f64>) -> State {
+    State(format!("{:?} x {}",parameters,state.0))
+}
+
+// You could imagine doing something like this too 
+// A command/instruction constructor 
+// fn rotate_by(axis: Vector, angle: Radians) -> Box<dyn Fn(& State, Vec<f64>) -> State> { }
+
+// For reference 
+// fn returns_closure() -> Box<dyn Fn(i32) -> i32> {
+//     Box::new(|x| x + 1)
+// }
+                                
+
